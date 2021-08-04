755fcd0b133fcecdd49b4eb8080888f26e2404e2
besherJ <besherjeiroudi@gmail.com>
2021-08-04 16:41:37 +0300

    add NotificationService Skeleton

---
+++ SmartStart.Repository.Setting/NotificationReposotiry.cs
@@ -0,0 +1,20 @@
+﻿using Elkood.Web.Service.BoundedContext;
+using SmartStart.Model.Setting;
+using SmartStart.SqlServer.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStart.Repository.Setting
+{
+    public class NotificationReposotiry : ElRepository<SmartStartDbContext, Guid, Notification>, INotificationRepository
+    {
+        public NotificationReposotiry(SmartStartDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
