503965e6ee107ee6da2b0ca6737ebcabaa5390b5
besherJ <besherjeiroudi@gmail.com>
2021-08-04 15:52:42 +0300

    add base services(city, university, notification) skeleton

---
+++ SmartStart.Repository.General/UniversityService/UniversityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Elkood.Web.Service.BoundedContext.General;
+using Elkood.Web.Service.DependencyInjection;
+using SmartStart.DataTransferObject.UniverstiyDto;
+using SmartStart.Model.General;
+using SmartStart.SqlServer.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStart.Repository.General.UniversityService
+{
+    [ElRepository]
+    public class UniversityRepository : ElRepositoryGeneral<SmartStartDbContext, Guid, University, UniversityDto>, IUniversityRepository
+    {
+        public UniversityRepository(SmartStartDbContext context) : base(context) { }
+    }
+}
--- SmartStart/Startup.cs
+++ SmartStart/Startup.cs
@@ -1,4 +1,6 @@
 using Elkood.Web.Domain.ConfigureServices;
+using Elkood.Web.Infrastructure.Configuration;
+using Elkood.Web.Service.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +15,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SmartStart.Model.Security;
 using SmartStart.SqlServer.DataBase;
+using SmartStart.SqlServer.DataBase.Seed;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
 using System.Collections.Generic;
@@ -54,7 +57,7 @@ namespace SmartStart
                 options.Password.RequireLowercase = false;
             }).AddEntityFrameworkStores<SmartStartDbContext>().AddDefaultTokenProviders();
 
-
+            services.AddElRepositoryInject("SmartStart.Repository.General");
 
             services.AddSpaStaticFiles(configuration: options => { options.RootPath = "clientApp"; });
             services.AddCors(options =>
@@ -166,6 +169,13 @@ namespace SmartStart
                                    forceKill: true,
                            wsl: false // Set to true if you are using WSL on windows. For other operating systems it will be ignored
                                    );
+            });
+
+            app.UseSqlServerSeed<SmartStartDbContext>(async (context, provider) => {
+                await context.Database.MigrateAsync();
+                await context.Database.EnsureCreatedAsync();
+                //await SecuritySeed.InitializeAsync(provider);
+                await DataSeed.InitializeAsync(provider);
             });
 
         }
