f7b0229127e7d59779fc3a03bd64022d40d8cda0
Osama Al-Rashed <Osama Al-Rashed@DESKTOP-8954161>
2021-08-05 19:23:44 +0300

    first step

---
+++ SmartStart.Repository.PointOfSale/DataTransferObject/CodeDto.cs
@@ -0,0 +1,24 @@
+﻿using SmartStart.SharedKernel.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStart.Repository.PointOfSale.PointOfSaleService.DataTransferObject
+{
+    public class CodeDto
+    {
+        public string Hash { get; set; }
+
+        public double Value { get; set; }
+
+        public double DiscountRate { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateActivated { get; set; }
+        public DateTime MaxEndDate { get; set; }
+        public CodeTypes Type { get; set; }
+        public bool IsInvoice { get; set; }
+
+    }
+}
