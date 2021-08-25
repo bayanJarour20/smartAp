c01033f7759f7e8f99bbef04e445bdab1c0e1389
Carol <Carol@DESKTOP-0KSSQU8>
2021-08-25 13:13:04 +0300

    ..

---
+++ SmartStart.SqlServer/Migrations/20210825100829_advertisementType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartStart.SqlServer.Migrations
+{
+    public partial class advertisementType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Type",
+                table: "Advertisements",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Advertisements");
+        }
+    }
+}
