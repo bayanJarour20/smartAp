67ec89fbf3ac076d27d5a96eba14333eb80cd425
Carol <Carol@DESKTOP-0KSSQU8>
2021-08-25 12:04:35 +0300

    add advertisementType

--- SmartStart.DataTransferObject/AdvertisementDto/AdvertisementDto.cs
+++ SmartStart.DataTransferObject/AdvertisementDto/AdvertisementDto.cs
@@ -1,5 +1,6 @@
 ﻿using Elkood.Web.Common.AuxiliaryImplemental.Interfaces;
 using SmartStart.Model.Setting;
+using SmartStart.SharedKernel.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,10 @@ namespace SmartStart.DataTransferObject.AdvertisementDto
         public string ImagePath { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public AdvertisementTypes Type { get; set; }
 
-        public static Expression<Func<Advertisement, AdvertisementDto>> Selector { get; set; } = advertisement => new AdvertisementDto() { Id = advertisement.Id, Title = advertisement.Title, ImagePath = advertisement.ImagePath, StartDate = advertisement.StartDate, EndDate = advertisement.EndDate };
-        public static Expression<Func<AdvertisementDto, Advertisement>> InverseSelector { get; set; } = advertisement => new Advertisement() { Id = advertisement.Id, Title = advertisement.Title, ImagePath = advertisement.ImagePath, StartDate = advertisement.StartDate, EndDate = advertisement.EndDate };
-        public static Action<AdvertisementDto, Advertisement> AssignSelector { get; set; } = (dto, entity) => { entity.Title = dto.Title; entity.ImagePath = dto.ImagePath; entity.StartDate = dto.StartDate; entity.EndDate = dto.EndDate; };
+        public static Expression<Func<Advertisement, AdvertisementDto>> Selector { get; set; } = advertisement => new AdvertisementDto() { Id = advertisement.Id, Title = advertisement.Title, ImagePath = advertisement.ImagePath, StartDate = advertisement.StartDate, EndDate = advertisement.EndDate, Type = advertisement.Type, };
+        public static Expression<Func<AdvertisementDto, Advertisement>> InverseSelector { get; set; } = advertisement => new Advertisement() { Id = advertisement.Id, Title = advertisement.Title, ImagePath = advertisement.ImagePath, StartDate = advertisement.StartDate, EndDate = advertisement.EndDate, Type = advertisement.Type, };
+        public static Action<AdvertisementDto, Advertisement> AssignSelector { get; set; } = (dto, entity) => { entity.Title = dto.Title; entity.ImagePath = dto.ImagePath; entity.StartDate = dto.StartDate; entity.EndDate = dto.EndDate; entity.Type = dto.Type; };
     }
 }
---
+++ SmartStart.SharedKernel/Enums/AdvertisementTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStart.SharedKernel.Enums
+{
+    public enum AdvertisementTypes
+    {
+        /// <summary>
+        /// عرض
+        /// </summary>
+        Offer,
+        /// <summary>
+        /// إعلان
+        /// </summary>
+        Advertisement,
+    }
+}
